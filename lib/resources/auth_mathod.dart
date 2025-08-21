c7e7b4650867cfd88ef618a956d25627a876a0d6
MohammadIrtisum <mdtanvir054321@gmail..com>
2025-08-21 22:08:23 +0600

    testing

--- lib/resources/auth_mathod.dart
+++ lib/resources/auth_mathod.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meassage_app/resources/storage_mathod.dart';
 import 'package:meassage_app/screen/signUp/sign_up.dart';
 
 class AuthMathod {
@@ -13,7 +14,7 @@ class AuthMathod {
     required String email,
     required String password,
     required String username,
-    // required Uint8List file,
+    required Uint8List file,
   }) async{
     String res = "Some error occured";
     try {
@@ -22,15 +23,19 @@ class AuthMathod {
       //   password: password,
       // );
       // res = "Success";
-      if( email.isNotEmpty  & email.contains("@") &&
-          password.isNotEmpty && password.length > 6 &&
-          username.isNotEmpty      ) {
+      if( email.isNotEmpty  ||
+          password.isNotEmpty ||
+          username.isNotEmpty  ||
+          file !=null
+            ) {
             // register user
        UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
           email: email,
           password: password,
         );
         print(userCredential.user!.uid);
+
+        String profilePicUrl = await StorageMethod().uploadImageToStorage("profilePic", file, false);
         // add user to database
         await _firestore.collection("users").doc(userCredential.user!.uid).set({
           "username": username,
@@ -38,6 +43,7 @@ class AuthMathod {
           "uid": userCredential.user!.uid,
           "followers": [],
           "following": [],
+          "profilePic": profilePicUrl,
         });
         res = "Success";
       } else {
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,13 +3,24 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-  pickImage(ImageSource source) async {
-  final ImagePicker _imagePicker = ImagePicker();
-  XFile?_file =  await _imagePicker.pickImage(source: source);
-  if( _file == null){
-    // return File ("_file.path");
-    return await _file?.readAsBytes();
+//   pickImage(ImageSource source) async {
+//   final ImagePicker _imagePicker = ImagePicker();
+//   XFile?_file =  await _imagePicker.pickImage(source: source);
+//   if( _file == null){
+//     // return File ("_file.path");
+//     return await _file?.readAsBytes();
+//   }
+//   print("NO Image Selected");
+// }
+
+class ImagePickerr {
+  Future<File>uploadImage(String inputSource) async {
+    final picker = ImagePicker();
+    final XFile? pickerImage  = await picker.pickImage(
+      source: inputSource == "camera" ? ImageSource.camera : ImageSource.gallery
+    );
+    File imageFile = File(pickerImage!.path);
+    return imageFile;
   }
-  print("NO Image Selected");
 }
    
